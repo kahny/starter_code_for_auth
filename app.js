68747e00694d1e40deb3dd850829a0ffba40249e
Connie Chang <changchilla@gmail.com>
2014-07-31 09:27:26 -0700

    initial push

--- app.js
+++ app.js
@@ -15,32 +15,57 @@ app.use(express.static(__dirname + '/public'));
 
 // logging
 app.use(function(req, res, next){
-  console.log(req.method, req.url)
-  next()
+  console.log(req.method, req.url);
+  next();
 });
 
+//list all blog posts in chronological order 
+app.get('/', function (req, res){
+  db.post.findAll().success(function(allPosts){
+    res.render('site/index', {posts: allPosts}); //the posts key is taco to be referenced in ejs 
+  });
+});
+
+//list all users
 app.get('/users', function (req,res) {
-//
+  db.user.findAll().success(function(foundUsers){
+    res.render('users/index', {users: foundUsers }); //first users is taco key which should match users ejs, second users is what's passed through the function 
+  });
 });
 
+//list all posts by specific user 
 app.get('/users/:id', function (req,res) {
-  //
+  var id = req.params.id; 
+  db.post.findAll({ where: {userId: id} }).success(function(foundPosts){
+    res.render('users/show', {posts: foundPosts, userId: id })
+    console.log("Found posts by author id")
+    console.log(foundPosts)
+  })
 });
 
+//list specific post
 app.get('/posts/:id', function (req,res) {
   var id = req.params.id;
+
   //
 
 });
 
+//create new post 
 app.get('/users/:id/posts/new', function(req, res){
   var id = req.params.id;
+  res.render('users/new', {userId: id})
   //
 });
+
 
 app.post('/users/:id/posts', function(req, res){
   var id = req.params.id;
-  //
+  var postTitle = req.body.postTitle;
+  var postBody = req.body.postBody;
+    db.post.create({ title: postTitle, body: postBody, userId: id }).success(function(post){
+      res.redirect('/blog')  //how would you redirect to /users/id ???
+    })
 });
 
 
